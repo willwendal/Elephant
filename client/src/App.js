3f7707f5bb80f8d6b645bfabac45586904e29fb1
will <william.jl.johnson@gmail.com>
2021-04-21 21:27:17 +0200

    landing page first styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import './App.scss';
+import elephantLogo from './elephant-logo.jpeg';
 
 function App() {
   return (
     <div className="app-container">
- 
+      <div className="app-elephant-logo">
+        <img src={elephantLogo} alt="elephant logo" />
+          <p className="slogan">Never Forget</p>
+      </div>
+      <div className="app-intro-text">
+        <p>Make sure never to miss another event, <br/>
+          Elephant will be here to remind you</p>
+      </div>
+      <div>
+        <button className="app-button">
+          Get Started
+        </button>
+      </div>
     </div>
   );
 }
