a3e12e82ce2fba05888e3c7105d65d6e47390de1
will <william.jl.johnson@gmail.com>
2021-04-26 19:05:28 +0200

    add event good *

--- client/src/Components/EventForm.js
+++ client/src/Components/EventForm.js
@@ -2,7 +2,7 @@ import './EventForm.scss';
 import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { getDetails } from '../services/APIservice';
-import { useParams } from 'react';
+
 
 
 function EventForm({postEvent}) {
@@ -48,30 +48,30 @@ function EventForm({postEvent}) {
   
       <form onSubmit={ submitAndNavigate }>
         <h1 className="form-event-header">Add Event</h1>
-        <div className="form-header-container">
+        
           <h3 className="form-header">
             Occasion
             <input className="input" onChange={(e) => {setOccasion(e.target.value)}} 
             value={occasion} 
             type="text"/>
           </h3>
-        </div>
-        <div className="form-location-container">
+        
+        
         <h3 className="form-location">
           Location
           <input className="input" onChange={(e) => {setLocation(e.target.value)}} 
           value={location} 
           type="text"/>
         </h3>
-        </div>
-        <div className="form-date-button-container">
+        
+        
         <h3 className="form-date">
           Date
           <input className="input-date" onChange={(e) => {setDate(e.target.value)}} 
           value={date} 
           type="date"/>
         </h3>
-        </div>
+        
         <div>
           <button className="submit-button"type="submit" onClick={submitAndNavigate} >Submit</button>
         </div>
