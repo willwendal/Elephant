29d7ee54562432f481c68d77ce09a48f9fe1e33a
Jonas-Petrauskas <tiesiogbicas@gmail.com>
2021-04-29 12:58:32 +0200

    fixed key in line 8

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -5,7 +5,7 @@ function Dashboard (props) {
   return (
     <div className='dashboard-container'>
       {props.eventData.map((event) => {
-        return <EventCard _id={event._id} occasion={event.occasion} date={event.date} />
+        return <EventCard key={event._id}  _id={event._id} occasion={event.occasion} date={event.date} />
       })}
     </div>
   )
