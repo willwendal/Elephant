74dd67470f0b8721c9b53e4f5272313e127ad361
Stefan Sarmir <stefansarmir@gmail.com>
2021-04-30 17:22:16 +0200

    deleted unnecessery tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -50,28 +50,5 @@ describe('Actions when clicking the sign in button', () => {
   });
 })
 
-describe('App rendering', () => {
-  test('Renders username input field', () => {
-    const userInputField = screen.getByTestId('usernameInput');
-    expect(userInputField).toBeInTheDocument();
-  });
-  test('Renders password input field', () => {
-    const passwordInputField = screen.getByTestId('passwordInput');
-    expect(passwordInputField).toBeInTheDocument();
-  });
-  test('renders Sign in button', () => {
-    const buttonElement = screen.getByText('Sign in');
-    expect(buttonElement).toBeInTheDocument();
-  });
-  test('types username input', () => {
-    const userInput = screen.getByTestId('usernameInput');
-    userEvent.type(userInput, 'Hello, World')
-    expect(userInput).toHaveValue('Hello, World')
-  });
-  test('types password input', () => {
-    const userInput = screen.getByTestId('passwordInput');
-    userEvent.type(userInput, 'Hello, World')
-    expect(userInput).toHaveValue('Hello, World')
-  });
-});
+
 
