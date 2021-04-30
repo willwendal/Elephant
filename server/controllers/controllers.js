9e4a50445fe674c5a3a4311880e600658e6b1705
Stefan Sarmir <stefansarmir@gmail.com>
2021-04-30 10:37:49 +0200

    refactor: refactored addEvent controller

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -18,13 +18,16 @@ async function getEventsByUid (req, res) {
 async function addEvent (req, res) {
   try {
     const { occasion, date, location } = req.body;
-    // console.log(occasion);
-    // console.log(date);
-    // console.log(location);
-    // await db.events.sync();
-    // const newEvent = await db.events.create({ occasion, date, location });
-    res.status(201);
-    res.send(newEvent);
+    if (occasion === '' || date === '' || location === '') {
+      res.status(400);
+      res.send('Data cant be an empty string');
+      throw new Error('Some of the received data is an empty string');
+    } else {
+      await db.events.sync();
+      const newEvent = await db.events.create({ occasion, date, location });
+      res.status(200);
+      res.send(newEvent);
+    }
   } catch (err) {
     console.log(err);
     res.status(400);
