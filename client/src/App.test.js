7d8e21e6564405d0de9deaf51661fb227175ffaa
Stefan Sarmir <stefansarmir@gmail.com>
2021-04-30 15:36:31 +0200

    test: added tests for log-in form

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,49 @@
-import { render, screen } from '@testing-library/react'
-import App from './App'
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+beforeEach(() => {
+  render(<App />);
+});
+
+describe('App rendering', () => {
+  test('Renders username input field', () => {
+    const userInputField = screen.getByTestId('usernameInput');
+    expect(userInputField).toBeInTheDocument();
+  });
+  test('Renders password input field', () => {
+    const passwordInputField = screen.getByTestId('passwordInput');
+    expect(passwordInputField).toBeInTheDocument();
+  });
+  test('renders Sign in button', () => {
+    const buttonElement = screen.getByText('Sign in');
+    expect(buttonElement).toBeInTheDocument();
+  });
+});
+
+describe('User types into input fields', () => {
+  test('Types inside the username input fields', () => {
+    const userInputField = screen.getByTestId('usernameInput');
+    userEvent.type(userInputField, 'testUsername');
+    expect(userInputField).toHaveValue('testUsername');
+  });
+  test('Types inside the password input fields', () => {
+    const passwordInputField = screen.getByTestId('passwordInput');
+    userEvent.type(passwordInputField, 'testPassword');
+    expect(passwordInputField).toHaveValue('testPassword');
+  });
+});
+
+describe('Actions when clicking the sign in button', () => {
+  test('If any of the input fields is empty, page should not redirect on click', () => {
+    const buttonElement = screen.getByText('Sign in');
+    const userInputField = screen.getByTestId('usernameInput');
+    const passwordInputField = screen.getByTestId('passwordInput');
+    expect(userInputField).toHaveValue('');
+    expect(passwordInputField).toHaveValue('');
+    userEvent.click(buttonElement);
+    expect(buttonElement).toBeInTheDocument();
+    expect(passwordInputField).toBeInTheDocument();
+    expect(userInputField).toBeInTheDocument();
+  });
 })
