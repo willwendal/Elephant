b626912c7016db09afa980ccf2362e7528289eb9
Jonas-Petrauskas <tiesiogbicas@gmail.com>
2021-05-05 10:59:59 +0200

    fix: unused variables in AddEvent

--- client/src/Pages/AddEvent/AddEvent.js
+++ client/src/Pages/AddEvent/AddEvent.js
@@ -1,15 +1,12 @@
 import './AddEvent.scss';
-import { useState } from 'react';
 import Navbar from '../../Components/NavBar/Navbar';
 import EventForm from '../../Components/EventForm/EventForm';
 import { postEventDB } from '../../services/APIservice';
 
 function AddEvent () {
-  const [event, setEvent] = useState([]);
 
   async function postEvent (newEvent) {
-    const createEvent = await postEventDB(newEvent);
-    setEvent(prevState => [...prevState, createEvent]);
+    await postEventDB(newEvent);
   }
 
   return (
