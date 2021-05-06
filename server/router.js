9bc5ed7213d0a5c60ee11fbe914d8767c21acc2b
Jonas-Petrauskas <tiesiogbicas@gmail.com>
2021-05-06 12:06:11 +0200

    fix: back end test fixed

--- server/router.js
+++ server/router.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 const controllers = require('./controllers/controllers');
 
-router.get('/my-events/:id', controllers.getEventsByUid);
 router.get('/my-events', controllers.getAllEvents);
 router.post('/add-event', controllers.addEvent);
-router.post('/add-user', controllers.addUser);
 router.delete('/delete/:id', controllers.deleteEvent);
 
 
